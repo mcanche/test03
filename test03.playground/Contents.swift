64376aa03269f2e296e4cd789faa62f32b913723
mcanche <mcanche@gmail.com>
2015-06-12 18:17:52 -0500

    test03
    playground - tutorial 03

---
+++ test03.playground/Contents.swift
@@ -0,0 +1,21 @@
+// +-------------+
+// | video 3     |
+// +-------------+
+
+
+
+import UIKit
+
+var fruit = "apples"
+var myfruit = "mango"
+
+switch fruit {
+    case "apples":
+        println("The fruit was some \(fruit)")
+    
+    case "mango":
+        println("The fruit was some \(fruit)")
+    default:
+        println("No fruit matched")
+    
+}
